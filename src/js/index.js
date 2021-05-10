f0cd63f7bcd7823e512368049de20fda0caf367a
Kateryna Liubchyk <shevtsova.kateryna@gmail.com>
2021-05-10 21:14:05 +0300

    fixing footer

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,7 @@
 import "@scripts/pagination"
 import '@scripts/BackToTopBtn'
 
+import '@scripts//modal-authorization/modal-authorization';
+import '@scripts/pagination';
+import '@scripts/backToTopBtn';
+import './team';
