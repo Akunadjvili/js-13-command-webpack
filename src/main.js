20cb8145653c1c01ebc01e064976bd4fb8690728
Holovatiuk Victor <Stethem696@gmail.com>
2021-05-19 08:57:58 +0300

    оформление селекта не законено

--- src/main.js
+++ src/main.js
@@ -4,62 +4,55 @@ import firebaseAPI from '@scripts/api/api.firebase.service';
 //do not touch this records
 import '@components/button.backtotop';
 
-
 import teamMarkUpTpl from '@templates/dynamic/team.info.hbs';
-import TeamModalForm from '@components/team.modal.form'
+import TeamModalForm from '@components/team.modal.form';
 
 import fabricPagination from '@components/pagination.fabric.js';
 import RandomEventProvider from '@providers/random.events.provider.js';
 import SearchEventProvider from '@providers/search.events.provider.js';
-
 
 import InfoEventModalForm from '@scripts/components/info.event.modal.form.js';
 import StandardFireBaseAuth from '@scripts/components/firebase.auth';
 
 import templateEventCard from '@templates/dynamic/event.info.modal.hbs';
 
-
 const teamLinkRef = document.querySelector('.team-link');
 teamLinkRef.addEventListener('click', openFormHandler);
 
 async function openFormHandler(event) {
-    event.preventDefault();
-    const modal = new TeamModalForm({
-        modal: '.js-modal',
-        template: teamMarkUpTpl,
-    })
-    await modal.show.bind(modal)();
+  event.preventDefault();
+  const modal = new TeamModalForm({
+    modal: '.js-modal',
+    template: teamMarkUpTpl,
+  });
+  await modal.show.bind(modal)();
 }
 
 const globalModal = new InfoEventModalForm({
-    modal: '.js-modal',
-    template: templateEventCard,
+  modal: '.js-modal',
+  template: templateEventCard,
 });
 
 window.eventModal = globalModal.openEvent.bind(globalModal);
 
-
 new StandardFireBaseAuth({ placeholder: '.header-auth', entry });
 async function entry() {
-    fabricPagination(RandomEventProvider, window.eventModal);
+  fabricPagination(RandomEventProvider, window.eventModal);
 }
-
-
-
 
 const queryInput = document.querySelector('.js-search-input');
 const searchForm = document.querySelector('.search-form');
 const countrySelect = document.querySelector('.js-country-select');
 
 const find = function (event) {
-    event.preventDefault();
-    const data = {
-        query: queryInput.value,
-        country: countrySelect.value
-    }
-    // console.log("HERE");
-    fabricPagination(SearchEventProvider, window.eventModal, data);
-}
+  event.preventDefault();
+  const data = {
+    query: queryInput.value,
+    country: countrySelect.value,
+  };
+  // console.log("HERE");
+  fabricPagination(SearchEventProvider, window.eventModal, data);
+};
 
 searchForm.addEventListener('submit', find);
 countrySelect.addEventListener('change', find);
